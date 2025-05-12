c0c5a37b2e64b100909469e8d927ebd3c7dd8f4b
BartekPachniak <bartekpachniak@gmail.com>
2025-05-12 15:40:15 +0200

    Chapter 10
    FINALNA WERSJA DZIALA WSZYSTKO

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from config import Config
-import logging
-from logging.handlers import SMTPHandler
-from logging.handlers import RotatingFileHandler
-import os
 from flask_mail import Mail
-
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -32,6 +31,7 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
     if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
@@ -43,6 +43,5 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
-    mail = Mail(app)
-from app import routes, models, errors
 
+from app import routes, models, errors
