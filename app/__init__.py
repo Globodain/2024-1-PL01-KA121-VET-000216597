9e286510ad2e44fa713ad3b8bf05963387ca99de
b-krol <bartosz.krol777@gmail.com>
2025-05-12 15:18:24 +0200

    Improved presentation of dates and time (chapter 12)

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 
 import logging
 from logging.handlers import SMTPHandler
@@ -20,6 +21,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+moment = Moment(app)
 
 
 if not app.debug:
