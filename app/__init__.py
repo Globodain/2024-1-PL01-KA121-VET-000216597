aa3e6f6cc0df00128aee5f89fc3dc7886a260268
b-krol <bartosz.krol777@gmail.com>
2025-05-12 15:40:59 +0200

    Included Flask-Babel in the application (chapter 13)

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
+from flask import request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel
 
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
+
+
+def get_locale():
+    #return 'pl'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -22,6 +30,7 @@ login.login_view = 'login'
 
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 
 if not app.debug:
--- readme.md
+++ readme.md
@@ -1,1 +1,18 @@
 Write here the final project report and how deploy your code
+
+
+
+
+
+------------------------------------------------
+Flask-Babel
+------------------------------------------------
+
+To add new language execute:
+flask translate init <language-code>
+
+To update languages execute:
+flask translate update
+
+To compile languages after updating execute:
+flask translate compile
